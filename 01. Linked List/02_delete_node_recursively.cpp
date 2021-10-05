797da63c7d7f73bec46794372ec2415da3ef1f19
moni-sinha <86065396+moni-sinha@users.noreply.github.com>
2021-10-05 19:52:21 +0530

    Add files via upload

---
+++ 01. Linked List/02_delete_node_recursively.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+	public:
+		int data;
+		Node* next;
+	Node(int data)
+	{
+		this->data=data;
+		next=NULL;
+	}
+};
+
+Node* insertNode(Node* head, int i, int data)
+{
+	if(head==NULL)
+		return head;
+	if(i==0)
+	{
+		Node* newNode=new Node(data);
+		newNode->next=head;
+		head=newNode;
+		return head;
+	}
+	Node* x=insertNode(head->next,i-1,data);
+	head->next=x;
+}
+
+Node* deleteNode(Node* head, int i)
+{
+	if(head==NULL)
+		return head;
+	if(i==0)
+	{
+		Node* temp=head;
+		head=head->next;
+		delete temp;
+		return head;
+	}
+	Node* x=deleteNode(head->next, i-1);
+	head->next=x;
+}
+
+void print(Node* head)
+{
+	Node* temp=head;
+	if(!temp)
+		cout<<"empty linked list";
+	while(temp)
+	{
+		cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+}
+int main()
+{
+	Node* head=NULL;
+	Node* newNode=new Node(4);
+	head=newNode;
+	
+	head=insertNode(head, 0, 0);
+	head=insertNode(head, 1, 3);
+	head=insertNode(head, 0, 5);
+	head=insertNode(head, 2, 9);
+	head=insertNode(head, 3, 7);
+	head=insertNode(head, 5, 1);
+	
+	print(head);
+	cout<<endl;
+	
+	head=deleteNode(head, 0);
+	print(head);
+	cout<<endl;
+	
+	head=deleteNode(head, 3);
+	print(head);
+	cout<<endl;
+	
+	head=deleteNode(head, 2);
+	print(head);
+	cout<<endl;
+	
+	head=deleteNode(head, 3);
+	print(head);
+	cout<<endl;
+	
+	head=deleteNode(head, 0);
+	print(head);
+	cout<<endl;
+	
+	head=deleteNode(head, 1);
+	print(head);
+	cout<<endl;
+	
+	head=deleteNode(head, 0);
+	print(head);
+	cout<<endl;
+	
+	/*
+	5 0 9 7 3 1 4
+	0 9 7 3 1 4
+	0 9 7 1 4
+	0 9 1 4
+	0 9 1
+	9 1
+	9
+	empty linked list
+	*/
+	return 0;
+}
